de89ec181bb5b853797e40bb22fce0eb9020ad6a
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2021-09-17 01:23:30 +0530

    Understanding websockets using socket.io

--- websockets/exercise-socketio/backend/server.js
+++ websockets/exercise-socketio/backend/server.js
@@ -7,8 +7,8 @@ const msg = new nanobuffer(50);
 const getMsgs = () => Array.from(msg).reverse();
 
 msg.push({
-  user: "brian",
-  text: "hi",
+  user: "abhu-A_J",
+  text: "This is the initial message",
   time: Date.now(),
 });
 
@@ -19,11 +19,31 @@ const server = http.createServer((request, response) => {
   });
 });
 
-/*
- *
- * Code goes here
- *
- */
+// create a new io server
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("Connected" + socket.id);
+
+  // writing off an event (there can be many events)
+  socket.emit("msg:get", { msg: getMsgs() });
+
+  // beneath socket.emit("msg:get")
+  socket.on("msg:post", (data) => {
+    msg.push({
+      user: data.user,
+      text: data.text,
+      time: Date.now(),
+    });
+
+    // io means the whole server
+    io.emit("msg:get", { msg: getMsgs() });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Disconnected" + socket.id);
+  });
+});
 
 const port = process.env.PORT || 8080;
 server.listen(port, () =>
